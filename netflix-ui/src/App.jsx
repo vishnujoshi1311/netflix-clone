5dc0bed2bd5fd155f2520938f400335c5cc8ca5c
Vishnu joshi <vishnuakj59@gmail.com>
2024-03-15 17:12:52 +0530

    minor changes

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup';
+import Netflix from './Pages/Netflix';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/Login" element={<Login />}/>
+        <Route exact path="/Signup" element={<Signup />}/>
+        <Route exact path="/" element={<Netflix />}/>
+      </Routes>
+      
+      </BrowserRouter>
+  );
 }
 
 export default App
